e185137cc1ff40e599021d52d68a0034abc20e74
huazai <who.seek.me@java98k.vip>
2019-01-26 17:30:46 +0800

    新增SpringCloudConfig客户端微服务配置模块

--- microservice-config-service-5001/src/main/java/com/huazai/springcloud/MicroserviceConfigServiceApp_5001.java
+++ microservice-config-service-5001/src/main/java/com/huazai/springcloud/MicroserviceConfigServiceApp_5001.java
@@ -10,21 +10,21 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  * @contact who.seek.me@java98k.vip
  *          <ul>
  * @description
- *              <li>服务提配置中心
+ *              <li>SpringCloudConfig服务端
  *              </ul>
- * @className MicroserviceConfigApp_5001
+ * @className MicroserviceConfigServiceApp_5001
  * @package com.huazai.springcloud
- * @createdTime 2019年1月6日 下午2:21:44
+ * @createdTime 2019年1月6日 下午5:24:36
  *
  * @version V1.0.0
  */
 @SpringBootApplication
 @EnableConfigServer
-public class MicroserviceConfigApp_5001
+public class MicroserviceConfigServiceApp_5001
 {
 
 	public static void main(String[] args)
 	{
-		SpringApplication.run(MicroserviceConfigApp_5001.class, args);
+		SpringApplication.run(MicroserviceConfigServiceApp_5001.class, args);
 	}
 }
