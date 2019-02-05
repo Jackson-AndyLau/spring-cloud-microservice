d9762a40f0e92d788f24fbdcac2c1e841747c061
huazai <who.seek.me@java98k.vip>
2019-02-05 21:56:12 +0800

    分布式配置中心实战-加入eureka服务器

---
+++ microservice-config-eureka-client-7001/src/main/java/com/huazai/springcloud/MicroserviceConfigEurekaClientApp_7001.java
@@ -0,0 +1,29 @@
+package com.huazai.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 
+ * @author HuaZai
+ * @contact who.seek.me@java98k.vip
+ *          <ul>
+ * @description
+ *              <li> Eureka Server 服务器端启动类,接受其它微服务注册进来
+ *              </ul>
+ * @className MicroserviceConfigEurekaClient_7001
+ * @package com.huazai.springcloud
+ * @createdTime 2018年10月5日 下午5:52:07
+ *
+ * @version V1.0.0
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class MicroserviceConfigEurekaClientApp_7001
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(MicroserviceConfigEurekaClientApp_7001.class, args);
+	}
+}
