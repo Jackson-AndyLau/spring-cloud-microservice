8023754755b550b9a2b3e451097eca64f4ac4ed6
huazai <who.seek.me@java98k.vip>
2019-02-06 19:58:19 +0800

    新增基于分布式配置中心的服务提供者模块

---
+++ microservice-config-provider-client-8001/src/main/java/com/huazai/springcloud/MicroserviceConfigProviderClientApp_8001.java
@@ -0,0 +1,32 @@
+package com.huazai.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * 
+ * @author HuaZai
+ * @contact who.seek.me@java98k.vip
+ *          <ul>
+ * @description
+ *              <li>服务提供者1号服务器
+ *              </ul>
+ * @className MicroserviceProviderApp_8001
+ * @package com.huazai.springcloud
+ * @createdTime 2019年1月6日 下午2:21:44
+ *
+ * @version V1.0.0
+ */
+@SpringBootApplication
+@EnableEurekaClient // 本服务启动后会自动注册进eureka服务中
+@EnableDiscoveryClient // 服务发现
+public class MicroserviceConfigProviderClientApp_8001
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(MicroserviceConfigProviderClientApp_8001.class, args);
+	}
+}
---
+++ microservice-config-provider-client-8001/src/main/java/com/huazai/springcloud/controller/DepartmentController.java
@@ -0,0 +1,54 @@
+package com.huazai.springcloud.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.huazai.springcloud.entity.Department;
+import com.huazai.springcloud.service.DepartmentService;
+
+@RestController
+@RequestMapping(value = "/department")
+public class DepartmentController
+{
+
+	@Autowired
+	private DepartmentService departmentService;
+
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public boolean add(@RequestBody Department department)
+	{
+//		Department department =  new Department(name);
+		return departmentService.add(department);
+	}
+
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
+	public void delete(@PathVariable Long id)
+	{
+		System.out.println(id);
+	}
+
+	@RequestMapping(value = "/update", method = RequestMethod.PUT)
+	public void update(@RequestBody Department department)
+	{
+
+	}
+
+	@RequestMapping(value = "/get/{id}", method = RequestMethod.GET)
+	public Department get(@PathVariable Long id)
+	{
+		return departmentService.get(id);
+	}
+
+	@RequestMapping(value = "/list")
+	public List<Department> list()
+	{
+		return departmentService.list();
+	}
+
+}
