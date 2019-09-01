286f8c3b1177e9a0dc5c58b175726b5e6061c44b
huazai <who.seek.me@java98k.vip>
2019-09-01 17:11:54 +0800

    新增数据库脚本儿

---
+++ microservice-api/src/main/java/com/huazai/springcloud/referenced document/microservice-01.sql
@@ -0,0 +1,40 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : 192.168.182.128
+ Source Server Type    : MySQL
+ Source Server Version : 50727
+ Source Host           : 192.168.182.128:3306
+ Source Schema         : microservice-01
+
+ Target Server Type    : MySQL
+ Target Server Version : 50727
+ File Encoding         : 65001
+
+ Date: 01/09/2019 17:09:53
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for department
+-- ----------------------------
+DROP TABLE IF EXISTS `department`;
+CREATE TABLE `department`  (
+  `deptno` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+  `dname` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '部门名称',
+  `db_source` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '数据库标识',
+  INDEX `主键索引`(`deptno`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1006 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of department
+-- ----------------------------
+INSERT INTO `department` VALUES (1001, '研发部', 'microservice-01');
+INSERT INTO `department` VALUES (1002, '业务部', 'microservice-01');
+INSERT INTO `department` VALUES (1003, '产品部', 'microservice-01');
+INSERT INTO `department` VALUES (1004, '财务部', 'microservice-01');
+INSERT INTO `department` VALUES (1005, '市场部', 'microservice-01');
+
+SET FOREIGN_KEY_CHECKS = 1;
